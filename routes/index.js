d633be0201c9d51fa708404e1879058472d7ad33
smardone <shaun.mardones@gmail.com>
2020-08-23 21:31:41 -0400

    reorganized code and created query functions to database

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.use(require('./department'));
-router.use(require('./employee'));
-router.use(require('./role'));
-
-module.exports = router;
--- server.js
+++ server.js
@@ -1,41 +1,89 @@
 const mysql = require('mysql2');
-const express = require('express');
-const app = express();
-const router = express.Router();
-const runProgram = require('./lib/index');
+const inquirer = require('inquirer');
 const PORT = process.env.PORT || 3001;
 
-// express middleware
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-app.use("/api", router);
 
 
 const connection = mysql.createConnection({
     host: 'localhost',
+    port: 3306,
     user: 'root',
     password: 'ThorLoki11!99',
+    database: 'roster'
 })
-
-createDatabase = () => {
-    connection.query('SOURCE db/schema.sql', () => {
-        console.log('Database generated');
-    })
-}
 
 connection.connect(err => {
     if (err) throw err;
     console.log('Connected!');
-    connectServer();
-    createDatabase();
-})
+    runProgram();
+});
 
-function connectServer() {
-    app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
-            runProgram();
-            });
+function runProgram() {
+
+    inquirer 
+        .prompt(
+            {
+                type: 'list',
+                name: 'action',
+                message: 'What would you like to do?',
+                choices: [
+                    'View All Departments',
+                    'View All Roles',
+                    'View All Employees',
+                    'Add a Department',
+                    'Add a Role',
+                    'Add an Employee',
+                    'Update an Employee Role'
+                ]
+            },
+            
+        ).then((answer) => {
+            console.log(answer);
+            if (answer.action === 'View All Departments') {
+                readDepartments();
+            } else if (answer.action === 'View All Roles') {
+                readRoles();
+            } else if (answer.action === 'View All Employees') {
+                readEmployees();
+            } else if (answer.action === 'Add a Department') {
+                inquirer
+                    .prompt({
+                        type: 'input',
+                        name: 'name',
+                        message: 'Enter the name of the new department'
+                    })
+                    .then((newDepartment) => {
+                        connection.query('INSERT INTO department SET ?', newDepartment, function(err, res) {
+                            if (err) throw err;
+                            console.log('Department has been created!');
+                        })
+                    })
+            }
+        
+        });
+    };
+
+
+function readDepartments()  {
+    connection.query('SELECT * FROM department', function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        connection.end();
+    })
 };
 
+function readRoles()  {
+    connection.query('SELECT * FROM role LEFT JOIN department ON department_id = department.id', function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        connection.end();
+    })
+};
 
-module.exports = { app, connection, router };
+function readEmployees()  {
+    connection.query('SELECT * FROM employee LEFT JOIN role ON role_id = role.id LEFT JOIN department ON department_id = department.id', function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        connection.end();
+    })
+};
